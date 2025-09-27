e0df6c776957e4bc2ee724849bfec9b208149815
dhananjreddy <dhananjreddy@gmail.com>
2025-09-27 12:18:42 +0000

    Handlers

--- golang/main.go
+++ golang/main.go
@@ -6,7 +6,12 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("content-type", "text/plain; charset=utf-8")
+		w.Write([]byte("OK"))
+	})
 
 	Server := &http.Server{
 		Addr:    ":8080",
