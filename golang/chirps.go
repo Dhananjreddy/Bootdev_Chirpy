003b8c34ad0d60cfa01754a310d4a5c820359c35
dhananjreddy <dhananjreddy@gmail.com>
2025-10-19 15:40:38 +0000

    Get all chirps

--- golang/chirps.go
+++ golang/chirps.go
@@ -11,18 +11,20 @@ import(
 
 )
 
+
+type newChirp struct {
+	Id 			uuid.UUID			`json:"id"`
+	CreatedAt   time.Time 			`json:"created_at"`
+	UpdatedAt   time.Time 			`json:"updated_at"`
+	Body 		string 				`json:"body"`
+	UserID		uuid.UUID			`json:"user_id"`
+}
+
 func (apiCfg *apiConfig) handlerCreateChirp(w http.ResponseWriter, r *http.Request){
+	
 	type chirpParams struct{
 		Body   string       `json:"body"`
 		UserID uuid.UUID	`json:"user_id"`
-	}
-
-	type newChirp struct {
-		Id 			uuid.UUID			`json:"id"`
-		CreatedAt   time.Time 			`json:"created_at"`
-		UpdatedAt   time.Time 			`json:"updated_at"`
-		Body 		string 				`json:"body"`
-		UserID		uuid.UUID			`json:"user_id"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -55,6 +57,27 @@ func (apiCfg *apiConfig) handlerCreateChirp(w http.ResponseWriter, r *http.Reque
 	})
 
 	return
+}
+
+func (apiCfg *apiConfig) handlerGetAllChirps(w http.ResponseWriter, r *http.Request){
+	chirps, err := apiCfg.db.GetAllChirps(r.Context())
+	if err != nil {
+		respondWithError(w, 500, "Error fetching chirps from database", err)
+	}
+
+	var chirpsOut []newChirp
+
+	for _, chirp := range chirps {
+		chirpsOut = append(chirpsOut, newChirp{
+		Id: chirp.ID,
+		CreatedAt: chirp.CreatedAt,
+		UpdatedAt: chirp.UpdatedAt,
+		Body: chirp.Body,
+		UserID: chirp.UserID,
+		})
+	}
+
+	respondWithJSON(w, 200, chirpsOut)
 }
 
 func validateChirp(chirp string) (string, error){
